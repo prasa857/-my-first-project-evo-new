0722aab05bcf7ce30067f37d28e023685251ef35
Buddhika <buddhika691@gmail.com>
2024-12-09 23:38:40 +0200

    dev update

---
+++ app/login/page.jsx
@@ -0,0 +1,12 @@
+import Loginform from './login form'
+
+//Server companent for SSR
+
+export default function page() {
+	return (
+		<div className="flex flex-col justify-center items-center min-h-screen text-black ">
+			<Loginform title="Sign in to EvoTech"/>
+		</div>
+
+	);
+}
