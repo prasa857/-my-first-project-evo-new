11b1fbf1a0d124fcb0579772d0ecd2852791e8a3
Buddhika <buddhika691@gmail.com>
2024-12-10 19:24:34 +0200

    Refactor login form

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,11 @@
-import Loginform from './login form'
+import Loginform from "./login-form";
 
 //Server companent for SSR
 
 export default function page() {
-	return (
-		<div className="flex flex-col justify-center items-center min-h-screen text-black ">
-			<Loginform title="Sign in to EvoTech"/>
-		</div>
-
-	);
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen text-black ">
+      <Loginform title="Sign in to EvoTech" />
+    </div>
+  );
 }
