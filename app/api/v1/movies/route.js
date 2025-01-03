1937c091efa51c4a9b82727ac11a1940ad9b2e58
Buddhika <buddhika691@gmail.com>
2025-01-03 10:51:03 +0200

    create dashboard page

--- app/api/v1/movies/route.js
+++ app/api/v1/movies/route.js
@@ -1,5 +1,15 @@
 import { NextResponse } from "next/server";
 
+const MOVIES = [
+  { id: 1, Title: "Harry Potter" },
+  { id: 2, Title: "Load of the Rings" },
+  { id: 3, Title: "Spider Man" },
+  { id: 4, Title: "X-MAN" },
+  { id: 5, Title: "Flash" },
+  { id: 6, Title: "Speed" },
+  { id: 7, Title: "Rambo" },
+];
+
 export const GET = async (req) => {
-  return NextResponse.json({ success: true, movies: [] });
+  return NextResponse.json({ success: true, movies: MOVIES });
 };
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,8 +1,12 @@
 import { getMovies } from "../libs/apis/server";
 
 export default async function DashboardPage() {
-  // 1.Add shadcn card
-  const movies = await getMovies();
+  // 1. Add shadcn card
+  // 2. Create movie Get EndPoint
+  // 3. Read the dummy response
+  // 4. Reader data set in  the UI
+
+  const { movies } = await getMovies();
   console.log("MOVIES::", movies);
 
   return (
@@ -16,14 +20,21 @@ export default async function DashboardPage() {
       {/*body section */}
       <div className="container m-8">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-          <div className="h-96 bg-green-400 ">Div 1</div>
+          {/*<div className="h-96 bg-green-400 ">Div 1</div>
           <div className="h-96 bg-yellow-400 ">Div 2</div>
           <div className="h-96 bg-blue-400 ">Div 3</div>
           <div className="h-96 bg-orange-400 ">Div 4</div>
           <div className="h-96 bg-red-400 ">Div 5</div>
           <div className="h-96 bg-purple-400 ">Div 6</div>
           <div className="h-96 bg-lime-400 ">Div 7</div>
-          <div className="h-96 bg-cyan-400 ">Div 8</div>
+          <div className="h-96 bg-cyan-400 ">Div 8</div>*/}
+
+          {movies?.length &&
+            movies.map((movie) => (
+              <div key={movie.id} className="h-96 bg-green-400 ">
+                {movie?.Title}
+              </div>
+            ))}
         </div>
       </div>
     </main>
