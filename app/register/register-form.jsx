f9c250e6edd501a2189c29b8a897646d8b2a55fa
Buddhika <buddhika691@gmail.com>
2025-01-24 20:45:33 +0200

    Created register form validation part

--- app/register/register-form.jsx
+++ app/register/register-form.jsx
@@ -1,5 +1,5 @@
 "use client";
-//keep this as the client component  (funtional component)
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import {
@@ -10,53 +10,91 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@radix-ui/react-label";
+import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 
-export default function RegisterForm() {
-  const handleSubmitForm = async (event) => {
-    event.preventDefault();
+const DEFAULT_ERROR = {
+  error: false,
+  message: "",
+};
 
-    console.log("Submitted");
+export default function RegisterForm() {
+  const [error, setError] = useState(DEFAULT_ERROR);
+
+  const handleSubmitForm = async (event) => {
+    event?.preventDefault();
+    const formData = new FormData(event?.currentTarget);
+    const name = formData.get("name") ?? "";
+    const email = formData.get("email") ?? "";
+    const password = formData.get("password") ?? "";
+    const confirmPassword = formData.get("confirm-password") ?? "";
+
+    if (name && email && password && confirmPassword) {
+      if (password === confirmPassword) {
+        setError(DEFAULT_ERROR);
+      } else {
+        setError({ error: true, message: "Passwords don't match" });
+      }
+    } else {
+      setError({ error: true, message: "All fields are required" });
+    }
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
-      <Card className="bg-blue-50/90  w-[350px]">
+      <Card className="bg-blue-50/90 w-[350px]">
         <CardHeader>
           <CardTitle>Create an account</CardTitle>
-          <CardDescription>Enter Your Information To Get Start</CardDescription>
+          <CardDescription>
+            Enter Your Information To Get Started
+          </CardDescription>
         </CardHeader>
         <form onSubmit={handleSubmitForm}>
           <CardContent>
-            {" "}
-            <div className="flex flex-col space y-1.5 ">
-              <div className="flex flex-col space-1.5 ">
+            <div className="flex flex-col space-y-1.5">
+              <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" name="name " placeholder="john Doe" />
+                <Input id="name" name="name" placeholder="John Doe" />
               </div>
-              <div className="flex flex-col space-1.5 ">
+              <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" name="email" placeholder="john@example.com" />
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="john@example.com"
+                />
               </div>
-              <div className="flex flex-col space-1.5 ">
+              <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   name="password"
+                  type="password"
                   placeholder="Enter Password"
                 />
               </div>
-              <div className="flex flex-col space-1.5 ">
-                <Label htmlFor="comfirm password">Comfirm Password</Label>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="confirm-password">Confirm Password</Label>
                 <Input
-                  id="comfirm password"
-                  name="comfirm password"
-                  placeholder="Enter comfirm password"
+                  id="confirm-password"
+                  name="confirm-password"
+                  type="password"
+                  placeholder="Enter confirm password"
                 />
               </div>
-              <div className="flex justify-center gap-2 text-xm">
-                Already as anaccont?
+
+              {/*form error*/}
+              <div className="flex justify-center">
+                {error?.error && (
+                  <span className="text-red-600 text-xs text-center">
+                    {error.message}
+                  </span>
+                )}
+              </div>
+
+              <div className="flex justify-center gap-2 text-sm">
+                Already have an account?{" "}
                 <Link href="/login" className="text-blue-600 hover:underline">
                   Login
                 </Link>
