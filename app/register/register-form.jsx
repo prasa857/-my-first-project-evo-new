4df153b6cdbfa2ef01dba17d908e348ad42e4665
Buddhika <buddhika691@gmail.com>
2025-02-25 22:16:36 +0200

    BetterAuth internal server error (500),corection code

--- app/register/register-form.jsx
+++ app/register/register-form.jsx
@@ -1,7 +1,9 @@
 "use client";
+
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { Loader2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -10,10 +12,11 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+// import { ToastAction } from "@/components/ui/toast";
+import { useToast } from "@/hooks/use-toast";
 // import { registerUser } from "@/lib/apis/server";
-
 import { signUp } from "@/lib/auth-client";
 
 const DEFAULT_ERROR = {
@@ -21,68 +24,95 @@ const DEFAULT_ERROR = {
   message: "",
 };
 
+// Keep this as a client component (functional component)
 export default function RegisterForm() {
   const [error, setError] = useState(DEFAULT_ERROR);
+  const [isLoading, setLoading] = useState(false);
+  const { toast } = useToast();
 
   const handleSubmitForm = async (event) => {
     event?.preventDefault();
     const formData = new FormData(event?.currentTarget);
     const name = formData.get("name").toString();
     const email = formData.get("email").toString();
-    const password = formData.get("password").toString();
+    const password = formData.get("password") ?? "";
     const confirmPassword = formData.get("confirm-password") ?? "";
 
-    //Basic FrontEnd Validation Logic
+    // console.log("Sumbitted!", { name, email, password, confirmPassword });
 
+    // Basic frontend validation logic
     // if (name && email && password && confirmPassword) {
     if (password === confirmPassword) {
       setError(DEFAULT_ERROR);
-      // await registerUser({ name, email, password });
+      // setLoading(true);
+      // const registerResp = await registerUser({ name, email, password });
+      // setLoading(false);
+
+      // if (registerResp?.error) {
+      //   setError({ error: true, message: registerResp.error });
+      // } else {
+      //   toast({
+      //     variant: "success",
+      //     title: "Registration successful!",
+      //     description: "Please continue with login",
+      //     action: (
+      //       <ToastAction altText="Login" className="hover:bg-green-700">
+      //         Login
+      //       </ToastAction>
+      //     ),
+      //   });
+      // }
 
       const { data, error } = await signUp.email(
         {
           email: email,
-          passwrod: password,
+          password: password,
           name: name,
           image: undefined,
         },
         {
           onRequest: () => {
-            // console.log("onRequest",ctx);
+            // console.log("onRequest", ctx);
           },
           onSuccess: (ctx) => {
             console.log("onSuccess", ctx);
           },
-          onError: (error) => {
-            console.log("onError", error);
+          onError: (ctx) => {
+            if (ctx) {
+              setError({ error: true, message: ctx.error.message });
+            }
           },
         }
       );
+
       if (data) {
         console.log("data", data);
       }
     } else {
-      setError({ error: true, message: "Passwords don't match" });
+      setError({ error: true, message: "Passwords doesn't match" });
     }
-    //}
+    // }
+
+    // console.log("Error!", error);
   };
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <Card className="bg-blue-50/90 w-[350px]">
         <CardHeader>
-          <CardTitle>Create an account</CardTitle>
-          <CardDescription>
-            Enter Your Information To Get Started
+          <CardTitle className="text-center">Create an account</CardTitle>
+          <CardDescription className="text-xs text-center">
+            Enter your information to get started
           </CardDescription>
         </CardHeader>
         <form onSubmit={handleSubmitForm}>
           <CardContent>
-            <div className="flex flex-col space-y-1.5">
+            <div className="flex flex-col space-y-4">
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="name">Name</Label>
                 <Input id="name" name="name" placeholder="John Doe" />
               </div>
+
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="email">Email</Label>
                 <Input
@@ -92,35 +122,37 @@ export default function RegisterForm() {
                   placeholder="john@example.com"
                 />
               </div>
+
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="password">Password</Label>
                 <Input
                   id="password"
                   name="password"
                   type="password"
-                  placeholder="Enter Password"
+                  placeholder="Enter new password"
                 />
               </div>
+
               <div className="flex flex-col space-y-1.5">
                 <Label htmlFor="confirm-password">Confirm Password</Label>
                 <Input
                   id="confirm-password"
                   name="confirm-password"
                   type="password"
-                  placeholder="Enter confirm password"
+                  placeholder="Enter password again to confirm"
                 />
               </div>
 
-              {/*form error*/}
+              {/* form errors */}
               <div className="flex justify-center">
                 {error?.error && (
-                  <span className="text-red-600 text-xs text-center">
+                  <span className="text-red-600 text-xs text-center animate-pulse duration-1000">
                     {error.message}
                   </span>
                 )}
               </div>
 
-              <div className="flex justify-center gap-2 text-sm">
+              <div className="flex justify-center gap-1 text-xs">
                 Already have an account?{" "}
                 <Link href="/login" className="text-blue-600 hover:underline">
                   Login
@@ -128,8 +160,10 @@ export default function RegisterForm() {
               </div>
             </div>
           </CardContent>
+
           <CardFooter className="flex justify-center">
-            <Button className="flex-1" type="submit">
+            <Button className="flex-1" type="submit" disabled={isLoading}>
+              {isLoading && <Loader2 className="animate-spin" />}
               Register
             </Button>
           </CardFooter>
